01a74105794dde64a5358d668e7574e839f4b5d0
SethMacP <macpherson.seth@gmail.com>
2021-02-25 14:21:34 -0800

    Model/Router complete: moving to middleware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,5 +1,33 @@
+//Readme Dump:
+// #### Write Middleware
+
+// - Write the following middlewares inside `api/accounts/accounts-middleware.js`:
+
+//   - `checkAccountPayload` returns a status 400 with if `req.body` is invalid:
+
+//     - If either name or budget are undefined, return `{ message: "name and budget are required" }`
+//     - If name is not a string, return `{ message: "name of account must be a string" }`
+//     - If the _trimmed_ name is shorter than 3 or longer than 100, return `{ message: "name of account must be between 3 and 100" }`
+//     - If budget is not a number, return `{ message: "budget of account must be a number" }`
+//     - If budget is a negative number or over one million, return  `{ message: "budget of account is too large or too small" }`
+
+//   - `checkAccountId` returns a status 404 with a 
+//          `{ message: "account not found" }` 
+//      if `req.params.id` does not exist in the database
+
+//   - `checkAccountNameUnique` returns a status 400 with a `{ message: "that name is taken" }` if the _trimmed_ `req.body.name` already exists in the database
+const model = require('./accounts-model');
+
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  if(!req.body.name || !req.body.budget){
+    return res.status(404).json({message:"Name & Budget are required"})
+  }
+  if(typeOf(req.body.name) != "string"){
+    return res.status(404).json({message:"Name must be a string"})
+  } 
+  next();
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
@@ -8,4 +36,12 @@ exports.checkAccountNameUnique = async (req, res, next) => {
 
 exports.checkAccountId = async (req, res, next) => {
   // DO YOUR MAGIC
+  model.getById(req.params.id)
+    .then(account=>{
+        req.account = account;
+        next();
+    })
+    .catch(()=>{
+        res.status(500).json({message:"Server Error"})
+    })
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,31 +1,69 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const model = require('./accounts-model');
+const {checkAccountId, checkAccountPayload, checkAccountNameUnique} = require('./accounts-middleware');
+const { resource } = require('../server');
 
 router.get('/', async (req, res, next) => {
-  // DO YOUR MAGIC
+	// DO YOUR MAGIC
+	model.getAll()
+		.then(accounts=>{
+			res.status(200).json(accounts)
+		})
+		.catch(()=>{
+			res.status(500).json({message:"Server Error"})
+		})
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id',  (req, res, next) => {
+	model.getById(req.params.id)
+		.then(account=>{
+			res.status(200).json(account)
+		})
+		.catch(()=>{
+			res.status(500).json({message:"Server Error"})
+		})
 })
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+	// DO YOUR MAGIC
+	console.log(req.body);
+	model.create(req.body)
+		.then(post=>{
+			res.status(200).json(post)
+		})
+		.catch(()=>{
+			res.status(500).json({message: "Server Error"})
+		})
 })
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+	// DO YOUR MAGIC
+	model.updateById(req.params.id, req.body)
+		.then(updatedPost => {
+			res.status(200).json(updatedPost)
+		})
+		.catch(()=>{
+			res.status(500).json({message:"Server Error"})
+		})
 });
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+	// DO YOUR MAGIC
+	model.deleteById(req.params.id)
+		.then(()=>{
+			res.status(200).json({message:"Deleted"})
+		})
+		.catch(()=>{
+			res.status(500).json({message:"Server Error"})
+		})
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // CALL next(err) IF THE PROMISE REJECTS INSIDE YOUR ENDPOINTS
-  res.status(500).json({
-    message: 'something went wrong inside the accounts router',
-    errMessage: err.message,
-  })
+	// CALL next(err) IF THE PROMISE REJECTS INSIDE YOUR ENDPOINTS
+	res.status(500).json({
+		message: 'something went wrong inside the accounts router',
+		errMessage: err.message,
+	})
 })
 
 module.exports = router;
